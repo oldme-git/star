a573602845e0d4985f3162a1a03fb47f7f940136
oldme <tyyn1022@gmail.com>
2024-11-07 16:43:27 +0800

    init

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"errors"
+
+	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
+	"github.com/gogf/gf/v2/frame/g"
 	_ "star/internal/packed"
 
 	"github.com/gogf/gf/v2/os/gctx"
@@ -9,5 +13,22 @@ import (
 )
 
 func main() {
+	var err error
+
+	// 检查数据库是否能连接
+	err = connData()
+	if err != nil {
+		panic(err)
+	}
+
 	cmd.Main.Run(gctx.GetInitCtx())
+}
+
+// connData 检查数据库连接是否正常
+func connData() error {
+	err := g.DB().PingMaster()
+	if err != nil {
+		return errors.New("连接到数据库失败")
+	}
+	return nil
 }
