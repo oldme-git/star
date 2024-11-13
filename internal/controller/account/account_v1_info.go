ebe6488385c752c53d41402b15ac4260a65b3d4e
oldme <tyyn1022@gmail.com>
2024-11-13 16:37:28 +0800

    up

--- internal/controller/account/account_v1_info.go
+++ internal/controller/account/account_v1_info.go
@@ -9,13 +9,13 @@ import (
 
 func (c *ControllerV1) Info(ctx context.Context, req *v1.InfoReq) (res *v1.InfoRes, err error) {
 	user, err := users.Info(ctx)
-	if err == nil {
-		res = &v1.InfoRes{
-			Username:  user.Username,
-			Email:     user.Email,
-			CreatedAt: user.CreatedAt,
-			UpdatedAt: user.UpdatedAt,
-		}
+	if err != nil {
+		return nil, err
 	}
-	return
+	return &v1.InfoRes{
+		Username:  user.Username,
+		Email:     user.Email,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}, nil
 }
