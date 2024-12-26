fafb380addbf0febbca0006c21f1c37a50bafea0
oldme <tyyn1022@gmail.com>
2024-12-26 10:38:15 +0800

    up

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -1,6 +1,5 @@
 package consts
 
 const (
-    // JwtKey in production, this value should be read from configuration.
-    JwtKey = "db03d23b03ec405793b38f10592a2f34"
+	JwtKey = "db03d23b03ec405793b38f10592a2f34"
 )
--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
-    _ "github.com/gogf/gf/contrib/drivers/mysql/v2"
+	"errors"
 
-    "star/internal/cmd"
+	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
 
-    "github.com/gogf/gf/v2/os/gctx"
+	"star/internal/cmd"
+
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gctx"
 )
 
 func main() {
-    cmd.Main.Run(gctx.GetInitCtx())
+	var err error
+
+	g.I18n().SetLanguage("zh-CN")
+
+	err = connDb()
+	if err != nil {
+		panic(err)
+	}
+
+	cmd.Main.Run(gctx.GetInitCtx())
+}
+
+func connDb() error {
+	err := g.DB().PingMaster()
+	if err != nil {
+		return errors.New("连接到数据库失败")
+	}
+	return nil
 }
